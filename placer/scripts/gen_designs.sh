8f7bcecb77f40c19eb43dd8ee04f74ea0f903090
Jeff Goeders <jeff.goeders@gmail.com>
2024-04-10 10:46:22 -0600

    Add placer design scripts

---
+++ placer/scripts/gen_designs.sh
@@ -0,0 +1,7 @@
+python3 gen_design.py ../circuits/small.txt 10 0.8 2 10 3 0
+python3 gen_design.py ../circuits/med1.txt 20 0.8 2 10 3 0
+python3 gen_design.py ../circuits/med2.txt 20 0.9 2 10 5 1
+python3 gen_design.py ../circuits/lg1.txt 30 0.8 2 10 3 0
+python3 gen_design.py ../circuits/lg2.txt 30 0.9 2 10 4 1
+python3 gen_design.py ../circuits/xl.txt 40 0.8 2 10 3 0
+python3 gen_design.py ../circuits/huge.txt 50 0.8 2 10 3 0
